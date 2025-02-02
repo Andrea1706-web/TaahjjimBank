678f8ee5ac687df6e12a82f999569e735617ae91
João Victor <joaovictorf01@gmail.com>
2025-02-02 14:07:53 -0300

    criando um projeto e adicionando o fluxo de criação de usuário.

---
+++ front/taahjjimbank-front/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { UserRegisterComponent } from './users/user-register/user-register.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterModule, HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'TaahjjimBank';
+}
---
+++ front/taahjjimbank-front/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { UserRegisterComponent } from './users/user-register/user-register.component';
+
+export const routes: Routes = [
+    { path: 'register', component: UserRegisterComponent }
+];

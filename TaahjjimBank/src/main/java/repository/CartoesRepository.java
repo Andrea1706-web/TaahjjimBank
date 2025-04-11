76d2926ab801a39fd78b2fbc32e792db7a588bdd
devHellen <hellenthaise@outlook.com>
2025-04-11 09:25:15 -0300

    Subindo atualizações LambdaHandler e DriverS3

--- TaahjjimBank/src/main/java/repository/CartoesRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.CartoesModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartoesRepository extends JpaRepository<CartoesModel, Long> {
-}

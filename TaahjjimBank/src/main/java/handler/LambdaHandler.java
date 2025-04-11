89e726aa2210f8878547a18874e413036073dfd8
devHellen <hellenthaise@outlook.com>
2025-04-11 14:38:27 -0300

    Subindo atualizações model

--- TaahjjimBank/src/main/java/handler/LambdaHandler.java
+++ TaahjjimBank/src/main/java/handler/LambdaHandler.java
@@ -3,34 +3,52 @@ package handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import model.CartoesModel;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.CartaoModel;
 import service.DriverS3;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class LambdaHandler implements RequestHandler<Map<String,Object>, String> {
 
     @Override
     public String handleRequest(Map<String, Object> event, Context context) {
-        CartoesModel cartao = new CartoesModel();
+        CartaoModel cartao = new CartaoModel();
+        cartao.setId("0");
+        cartao.setNumeroCartao("1234-1236-1235-1236");
         cartao.setValidade("12/25");
         cartao.setCodigo("123");
         cartao.setNumeroConta("987654321");
 
         //Define o nome do bucket e a chave para o objeto
-        String bucketName = "seu-bucket-s3";
+        String bucketName = "zupbankdatabase";
         String key = "dados/cartao-teste.json";
 
         //Cria uma instância do Driver S3 para CartaoModel
-        DriverS3<CartoesModel> drivesS3 = new DriverS3<>(bucketName, CartoesModel.class);
+        DriverS3<CartaoModel> driverS3 = new DriverS3<>(bucketName, CartaoModel.class);
 
         //Salva o objeto CartaoModel no S3
         try {
-            drivesS3.save(key, cartao);
-            return "Cartao salvo com sucesso no S3";
+            driverS3.save(key, cartao);
         } catch (Exception e) {
-            return "Erro ao salvar cartão no S3: " + e.getMessage();
+            throw e;
         }
+        try {
+            Optional<CartaoModel> cartao2 = driverS3.read(key);
+            if (cartao2.isPresent()) {
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.writeValueAsString(cartao2.get());
+            }
+        } catch (JsonProcessingException e) {
+            // Trate a exceção de processamento JSON aqui
+            e.printStackTrace();
+            return "Erro ao processar JSON";
 
+        } catch (Exception e) {
+            throw e;
+        }
+        return "Objeto não encontrado";
     }
 }

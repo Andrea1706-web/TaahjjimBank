85d8bde41aa4ec2115f2d463b69779f3df4e7f0f
Flavia <fghespanhol@hotmail.com>
2025-04-29 11:36:58 -0300

    fix: coloca a variável path como atributo da classe ContaBancariaService, e mudança do tipo para double(atributo saldo) na ContaBancariaModel

--- TaahjjimBank/src/main/java/service/ContaBancariaService.java
+++ TaahjjimBank/src/main/java/service/ContaBancariaService.java
@@ -12,6 +12,7 @@ import java.util.Map;
 public class ContaBancariaService {
     private final DriverS3<ContaBancariaModel> driverContaBancaria;
     private final ObjectMapper objectMapper;
+    private final String PATH = "dados/contaBancaria/";
 
     public ContaBancariaService(String bucketName) {
         this.driverContaBancaria = new DriverS3<>(bucketName, ContaBancariaModel.class);
@@ -23,13 +24,12 @@ public class ContaBancariaService {
                 null,
                 (int) payload.get("agencia"),
                 (String) payload.get("numeroCC"),
-                (float) payload.get("saldo"),
+                (double) payload.get("saldo"),
                 (String) payload.get("cpf"),
                 (TipoConta) payload.get("tipoConta")
         );
 
-        String path = "dados/contaBancaria/";
-        String key = path + contaBancaria.getNumeroCC() + ".json";
+        String key = PATH + contaBancaria.getNumeroCC() + ".json";
         driverContaBancaria.save(key, contaBancaria);
         return objectMapper.writeValueAsString(contaBancaria);
     }

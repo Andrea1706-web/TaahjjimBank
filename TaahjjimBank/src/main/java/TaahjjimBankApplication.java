49a6c6fa7550ce29887de40c9f994b25175878ac
thaisbbreder <thaisbragabreder@gmail.com>
2025-04-30 16:08:09 -0300

    corrige erro na main

--- TaahjjimBank/src/main/java/TaahjjimBankApplication.java
+++ TaahjjimBank/src/main/java/TaahjjimBankApplication.java
@@ -1,9 +1,5 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class TaahjjimBankApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TaahjjimBankApplication.class, args);
+             System.out.println("Iniciando projeto...");
     }
 }

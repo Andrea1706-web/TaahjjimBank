8bed6f944ec64518be70c982d5f7925469093d8b
Flavia <fghespanhol@hotmail.com>
2025-07-21 14:46:22 -0300

    modifica secret key

--- TaahjjimBank/src/main/java/util/JwtUtil.java
+++ TaahjjimBank/src/main/java/util/JwtUtil.java
@@ -7,7 +7,9 @@ import java.security.Key;
 import java.util.Date;
 
 public class JwtUtil {
-    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+//    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final String SECRET_KEY = "6v3sY8T+KpX1oQkFz2YfJw4vNrbh7+X2bWjNQ0Z9+Yk=";
+
 
     public static String generateToken(String username) {
         long expirationMillis = 1000 * 60 * 60; // 1 hora
@@ -15,7 +17,7 @@ public class JwtUtil {
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expirationMillis))
-                .signWith(key)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
 }

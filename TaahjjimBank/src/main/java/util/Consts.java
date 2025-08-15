6d1564acd4a3e936776458ce4578e91f9779fbdc
Flavia <fghespanhol@hotmail.com>
2025-08-15 16:09:39 -0300

    correções

--- TaahjjimBank/src/main/java/util/Consts.java
+++ TaahjjimBank/src/main/java/util/Consts.java
@@ -2,8 +2,8 @@ package util;
 
 public class Consts {
     public static final String BUCKET = "zupbankdatabase";
-    public static final String PATH_TRANSACAO = "dados/transacao/";
-    public static final String PATH_TRANSACAO_AGENDADA = "dados/transacaoAgendada/";
+    public static final String PATH_BUCKET_TRANSACAO = "dados/transacao/";
+    public static final String PATH_BUCKET_TRANSACAO_AGENDADA = "dados/transacaoAgendada/";
     public static final String PATH_USUARIO = "dados/usuario/";
     public static final String PATH_LOGIN = "dados/login/";
     public static final String PATH_CONTA_BANCARIA = "dados/contaBancaria/";
